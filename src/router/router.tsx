b204f6892d0850f76b08c1755aa3a656bb8d0147
pavel <pavel.yaroslavovich22@gmail.com>
2023-08-26 19:14:52 +0500

    com

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,15 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import SignInForm from "../components/signInForm/SignInForm";
 import Registration from "../components/registration/Registration";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SignInForm />,
-  },
-  {
-    path: "registration",
-    element: <Registration />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <SignInForm />,
+    },
+    {
+      path: "registration",
+      element: <Registration />,
+    },
+  ],
+  { basename: "/" }
+);
 
 export default router;
